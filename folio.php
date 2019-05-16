7a76ee99a1553e49041eb2b8fe256f315ec063f6
olacodeX <aluko798@gmail.com>
2019-05-16 22:01:17 +0100

    22.3

--- folio.php
+++ folio.php
@@ -34,11 +34,9 @@ if(isset($_POST['contact']))
 {
 $to_1 ='aluko798@gmail.com';
 $name = $_POST['name'];
+$Pnum = $_POST['P.num'];
 $subject_1 = 'Service request from '.$name;
 $email =$_POST['email'];
-$service = $_POST['service'];
-$contact = $_POST['contact'];
-$other = $_POST['other'];
 $headers_1 = "From: ".strip_tags($email) . "\r\n";
 $headers_1 .= "Reply-To: ".strip_tags($email). "\r\n";
 $headers_1 .= "CC: olawale.aluko@st.futminna.edu.ng \r\n";
@@ -61,6 +59,7 @@ $message_1 .= '<div class="panel-heading"><h3 style="text-align:center; color: r
 $message_1 .= '<div class="panel-body" style="color: rgb(228, 173, 107);">';
 $message_1 .='<p> "'.$name.'"</p>';
 $message_1 .= '<p> "'.$email.'"</p>';
+$message_1 .= '<p> "'.$Pnum.'"</p>';
 $message_1 .= '<p> "'.$comment.'"</p>';
 $message_1 .='</div>';
 $message_1 .='</div>';
@@ -68,20 +67,20 @@ $message_1 .= '</body></html>';
 //send mail
 if(mail($to_1, $subject_1, $message_1, $headers_1))
 {
-    echo "Thanks for patronizing us, Your satisfaction 
-    is important to us! <br>"."We will get back to you soonest.";
+    echo "Thanks for contacting me, Your satisfaction 
+    is important to me! <br>"."I will get back to you soonest.";
 }
 else
 {
-    echo "oops, sorry we can't process your request at this time <br>"."Please try again, Thanks.";
+    echo "oops, sorry your request can't processed at this time <br>"."Please try again, Thanks.";
 }
 
 
 
-$subject_2 = 'Thanks for contacting us.';
+$subject_2 = 'Thanks for contacting me.';
 $email =$_POST['email'];
 $to_2 =$email;
-$headers_2 = "From: ".strip_tags('SEGAINElectricals') . "\r\n";
+$headers_2 = "From: ".strip_tags('.olacodeX') . "\r\n";
 $headers_2 .= "Reply-To: ".strip_tags('aluko798@gmail.com'). "\r\n";
 $headers_2 .= "MIME-Version: 1.0\r\n";
 $headers_2 .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
@@ -98,10 +97,10 @@ $message_2 = '<html><head>
 $message_2 .='<div class="panel panel-default" style="background: black;">';
 $message_2 .='<div class="panel-heading" style="padding:3px; margin-bottom:10px; color: rgb(228, 173, 107);"><h3>Thank you for contacting us.</h3></div>';
 $message_2 .='<div class="panel-body" style="margin-bottom:10px; margin-top:10px; background: Aliceblue;">';
-$message_2 .= '<p> We have received your message and we are currently working on it,We will get back to you soon. </p>';
+$message_2 .= '<p> OlacodeX has received your message and currently working on it,he will get back to you soon. <br> Thanks </p>';
 $message_2 .='</div>';
 $message_2 .= '<div class="panel-footer" style:padding:10px; color: rgb(228, 173, 107);>';
-$message_2 .= '<p>Copyright &copy; 2018 | <a style="text-decoration:none;" href="elect.html">HOME</a> | All Rights Reserved |<a style="text-decoration:none;" href="elect.html/#about">About</a></p>';
+$message_2 .= '<p>Copyright &copy; 2018 | <a style="text-decoration:none;" href="https://afrifashionshop.000webhostapp.com/index.html">HOME</a> | All Rights Reserved |<a style="text-decoration:none;" href="https://afrifashionshop.000webhostapp.com/aboutme.html">About</a></p>';
 $message_2 .= '</div>';
 $message_2 .= '</div>';
 $message_2 .= '</body></html>';
@@ -111,8 +110,7 @@ mail($to_2, $subject_2, $message_2, $headers_2);
 }
 ?>
 <br><br>
-<button type="button" class="btn btn-sm btn-success"><a href="electhire-1.html" style="text-decoration:none; color:aliceblue;">Previous page</a> </button> | <button type="button" class="btn btn-sm btn-success"><a href="elect.html" style="text-decoration:none; color:aliceblue;">HOME</a> </button>
+<button type="button" class="btn btn-sm btn-success"><a href="aboutme.html" style="text-decoration:none; color:aliceblue;">Previous page</a> </button> | <button type="button" class="btn btn-sm btn-success"><a href="index.html" style="text-decoration:none; color:aliceblue;">HOME</a> </button>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-?>
